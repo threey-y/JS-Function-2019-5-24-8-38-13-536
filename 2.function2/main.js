9bfa9e20df6796e4e99915f66e52c5e97cf88835
threey-y <52376594+threey-y@users.noreply.github.com>
2019-07-14 17:15:46 +0800

    Create main.js

---
+++ 2.function2/main.js
@@ -0,0 +1,9 @@
+function palindrome(message){
+            for(var i = 0;i < message.length/2;i++){
+                if(message.charAt(i) != message.charAt(message.length-i-1)){
+                    return false;
+                }
+            }
+            return true;
+        }
+ console.log(palindrome('abcde'));
